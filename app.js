9e557dfb5f2e223a887a4a41c523f7b386d24d4d
Andrew Weiss <wandrew8@gmail.com>
2020-03-18 10:18:09 -0700

    styling added

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const searchUrl = "business&apiKey=1607dbe0f68548328a153148fe3b9431";
 const apiKey = `&apikey=${api_key}`;
 
 const images = {
-    health: "https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+    health: "https://images.unsplash.com/photo-1477332552946-cfb384aeaf1c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
     business: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    technology: "https://images.unsplash.com/photo-1455165814004-1126a7199f9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    science: "https://images.unsplash.com/photo-1534777410147-084a460870fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60",
+    technology: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    science: "https://images.unsplash.com/photo-1475906089153-644d9452ce87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
     entertainment: "https://images.unsplash.com/photo-1445985543470-41fba5c3144a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    sports: "https://images.unsplash.com/photo-1558006252-bc08d8bb0cf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    sports: "https://images.unsplash.com/photo-1556719779-e1413cb43bb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
 }
 
 const links = document.querySelectorAll('.link');
@@ -43,19 +43,22 @@ function searchBySection(section) {
 }
 
 function postArticles(data) {
+    console.log(data)
     let html = ''
     const articleContainer = document.querySelector('.articles');
     data.forEach(article => {
         html += `
             <div class="articleCard">
-                <img src="${article.urlToImage}" alt="${article.title}">
+                <img src="${article.urlToImage ? article.urlToImage : "https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"}" alt="${article.title}">
                 <h2>${article.title}</h2>
-                <p class="author">${article.author}</p>
+                <p class="author">${article.author ? article.author: ''}</p>
                 <hr>
-                <p class="description>${article.description}</p>
-                <a src="${article.url}">Read More...</a>
+                <p class="description">${article.description}</p>
+                <a href="${article.url}">Read More...</a>
             </div>`;
         articleContainer.innerHTML = html;
     })
 }
+
+
 
